d2a696373672fa333ec763959183e3f7b728e005
Andrew Austin <acaustin@uark.edu>
2013-10-03 05:26:07 +0000

    Initial model revisions for the Recruitment app.

--- evewspace/Recruitment/models.py
+++ evewspace/Recruitment/models.py
@@ -16,33 +16,29 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from django.db import models
 from django.contrib.auth.models import User
+
+from core.models import Corporation, Alliance
 # Create your models here.
-
-class Interest(models.Model):
-    """Represents an option for the 'What are you interests? question'"""
-    name = models.CharField(max_length=100)
-
 
 class Action(models.Model):
     """Represents an action that can be taken on an application e.g Intel Ran"""
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
+    descripiton = models.TextField(blank=True)
+    required = models.BooleanField(default=False)
 
 
 class Application(models.Model):
     """Represents a recruitment application."""
     applicant = models.OneToOneField(User, related_name="application", primary_key=True)
     timestamp = models.DateTimeField()
-    interests = models.ManyToManyField(Interest)
-    killboard = models.CharField(max_length=100)
     #closetime = None indicates that the application is still open
-    closetime = models.DateTimeField(blank=True, null=True)
-    disposition = models.IntegerField(choices=((0,'Duplicate'), (1,'Accepted'),
+    closetime = models.DateTimeField(null=True)
+    disposition = models.IntegerField(null=True, choices=((0,'Duplicate'), (1,'Accepted'),
         (2,'Rejected'), (3, 'Deferred')))
-    intelclear = models.DateTimeField()
-    standingsclear = models.BooleanField(default=False)
 
     class Meta:
-        permissions = (('can_recruit', 'Can view applications'),)
+        permissions = (('can_recruit', 'Can view applications'),
+                ('recruitment_admin', "Can administer the RO tool."))
 
     def __unicode__(self):
         return 'Applicant: %s Status: %s' % (self.applicant.name, self.disposition)
@@ -53,7 +49,7 @@ class AppVote(models.Model):
     application = models.ForeignKey(Application, related_name='votes')
     vote = models.ForeignKey(User, related_name='appvotes')
     disposition = models.IntegerField(choices=((1,'Accept',), (2,'Reject'), (3, 'Defer')))
-    note = models.TextField()
+    note = models.TextField(blank=True, null=True)
     timestamp = models.DateTimeField()
 
 
@@ -76,6 +72,15 @@ class Interview(models.Model):
 class AppQuestion(models.Model):
     """Represents a question to be asked on the application."""
     question = models.CharField(max_length=255)
+    question_type = models.IntegerField(choices=((1,'Text Field'),
+        (2, 'Text Box'), (3, 'Dropdown'), (3, 'Select'), (4, 'Multi-Select')))
+    required = models.BooleanField(default=True)
+    app_type = models.ForeignKey('AppType', related_name='questions')
+    app_stage = models.ForeignKey('AppStage', related_name='questions')
+    order = models.IntegerField()
+
+    class Meta:
+        ordering = ['order',]
 
 
 class AppResponse(models.Model):
@@ -85,12 +90,34 @@ class AppResponse(models.Model):
     response = models.TextField(blank=True, null=True)
 
 
+class AppQuestionChoice(models.Model):
+    question = models.ForeignKey(AppQuestion, related_name='choices')
+    value = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True, null=True)
+
+
+class AppType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True, blank=True)
+    use_standings = models.ForeignKey(Corporation,
+            related_name="applications", null=True)
+
+
+class AppStage(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True, blank=True)
+    app_type = models.ForeignKey(AppType, related_name='stages')
+    order = models.IntegerField()
+
+    class Meta:
+        ordering = ['order',]
+
+
 class StandigsRequirement(models.Model):
     """Represents a standing to be checked against applications."""
     entity = models.CharField(max_length=100)
-    #If standing is null and we have a requirement record, we can interpret this as
-    #requiring no standing
+    # If standing is null and we have a requirement record, we can interpret
+    # this as requiring no standing
     standing = models.FloatField(blank=True, null=True)
-    entitytype = models.IntegerField(choices=((0,'Corporation'), (1,'Faction')))
-
-
+    entitytype = models.IntegerField(choices=((0,'Corporation'),
+        (1,'Faction')))
